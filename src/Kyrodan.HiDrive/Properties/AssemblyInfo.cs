5d36ecddeab173ea79162e105073653a75095c76
Daniel Bölts <daniel@boelts.de>
2016-10-20 16:38:48 +0200

    Updated Build-System

--- src/Kyrodan.HiDrive/Properties/AssemblyInfo.cs
+++ src/Kyrodan.HiDrive/Properties/AssemblyInfo.cs
@@ -8,25 +8,4 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("HiDrive .Net SDK")]
 [assembly: AssemblyDescription("Integrates HiDrive into your C# app")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Daniel Bölts")]
-[assembly: AssemblyProduct("Kyrodan.HiDrive")]
-[assembly: AssemblyCopyright("Copyright © Daniel Bölts")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
-[assembly: AssemblyInformationalVersion("0.2.0-alpha")]
 
