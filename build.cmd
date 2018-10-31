9468cc097c89e10b31268deddbfdd846e13798da
Pascal Schumann <pascal.schumann@systecs.com>
2018-10-31 13:16:36 +0100

    Updating build script to use dotnet cli

--- build.cmd
+++ build.cmd
@@ -1,13 +1,8 @@
 @echo off
 set zip="tools\7za.exe"
-set nuget="tools\NuGet.exe"
-rem set msbuildenv="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
-set msbuildenv="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-
-if not exist %msbuildenv% goto error
-call %msbuildenv%
 
 :cleanup
+dotnet clean -c Release
 if not exist build mkdir build
 del /s /f /q build\*
 
@@ -15,21 +10,21 @@ if not exist build\bin mkdir build\bin
 if not exist build\dist mkdir build\dist
 
 :build
-%nuget% restore
-msbuild Kyrodan.HiDriveSdk.sln /p:Configuration=Release /t:Clean,Build /fl /flp:logfile=build\build.log
+dotnet build -c Release
 if %errorlevel% NEQ 0 goto error
 
 :test
-vstest.console /logger:Console src\Kyrodan.HiDrive.Tests\bin\Release\Kyrodan.HiDrive.Tests.dll 
+dotnet test src\Kyrodan.HiDrive.Tests\Kyrodan.HiDrive.Tests.csproj -c Release
 rem > build\testresults.txt
 if %errorlevel% NEQ 0 goto error
 
 
 :package
-xcopy src\Kyrodan.HiDrive\bin\Release\*.* build\bin
+dotnet pack src\Kyrodan.HiDrive\Kyrodan.HiDrive.csproj --no-build -c Release -o ..\..\build\dist
+
+xcopy src\Kyrodan.HiDrive\bin\Release\netstandard2.0\*.* build\bin
 rem del build\bin\*.pdb build\bin\*.xml 
 %zip% a -tzip build\dist\Kyrodan.HiDriveSDK.zip .\build\bin\*
-%nuget% pack src\Kyrodan.HiDrive\Kyrodan.HiDrive.csproj -Properties "Configuration=Release;Platform=AnyCPU" -OutputDirectory build\dist
 
 
 
